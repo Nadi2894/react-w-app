970f32d40a2121eb3ff5205c9f5ecc98ad6b7202
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-03-24 15:19:12 +0100

    Added new components,Axios, HTML,

---
+++ src/Weather.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./Weather.css";
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
+
+export default function Weather(props) {
+  let [weatherData, setweatherData] = useState({ loaded: false });
+  let [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setweatherData({
+      loaded: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    let apiKey = "b9ba0314a93083136d968577c718e31d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.loaded) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="Sumbit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
