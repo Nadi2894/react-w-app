964f949a8e9f0ebb57d6e615b1697bb335cddeef
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-03-24 16:53:02 +0100

    Added icon

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="App container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Tokyo" />
         <footer>
           This page was made by Nadira-Begim and is {""}
           <a href="https://github.com/Nadi2894/react-weather-appp">
