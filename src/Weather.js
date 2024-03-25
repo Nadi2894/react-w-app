7166972c87b203114f55eb3ef65113b9fd7995b5
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-03-25 15:06:49 +0100

    submit error solved

--- src/Weather.js
+++ src/Weather.js
@@ -50,7 +50,7 @@ export default function Weather(props) {
             </div>
             <div className="col-3">
               <input
-                type="Sumbit"
+                type="submit"
                 value="Search"
                 className="btn btn-primary w-100"
               />
